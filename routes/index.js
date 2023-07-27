3f19972fc6ad8d43032f72d9c0fd090cf3b0af6d
Dabana Intenque <64395601+dabanaluintenque@users.noreply.github.com>
2023-07-27 14:39:14 -0400

    moving folder to fix router

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,10 @@ var router = express.Router();
 var app = express();
 var path = require('path');
 
-/* GET home page. 
+//GET home page. 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});*/
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/Tutorial/', 'index.html'))
 });
+
 
 module.exports = router;
