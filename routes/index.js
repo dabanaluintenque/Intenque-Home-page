58eb41edbf881b8f6507ac87aa8361002f9e28d4
Dabana Intenque <64395601+dabanaluintenque@users.noreply.github.com>
2023-07-27 14:22:05 -0400

    new

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });*/
 
-app.use(express.static(path.join(__dirname,"./my-homepage")));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, './my-homepage', 'index.html'))
+app.use(express.static(path.join(__dirname,"./my-homepage/build")));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './my-homepage/build', 'index.html'))
 });
 
 module.exports = router;
