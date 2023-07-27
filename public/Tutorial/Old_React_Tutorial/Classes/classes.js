bc72e097703612b18da000852f235f93f62e56ef
Dabana Intenque <64395601+dabanaluintenque@users.noreply.github.com>
2023-07-27 12:29:02 -0400

    about / tutorial

---
+++ public/Tutorial/Old_React_Tutorial/Classes/classes.js
@@ -0,0 +1,32 @@
+
+class V{
+
+    construction(name){
+        this.name= name;
+   }
+}
+
+let mynew = new V("directToBissau");
+
+ //myVessel.vesselName;
+
+console.log( mynew.name);
+
+/*Ret = () => {
+    name1;
+
+
+function ReturnName(){
+
+    return " Dabana" +name1;
+}
+
+ReactDOM.render(
+
+    <ReturnName/>,
+    document.getElementById('myClass')
+);}*/
+
+//console.log("myVess"+myVessel.vesselName); 
+
+
---
+++ public/Tutorial/Old_React_Tutorial/getting_Start/gettingstart.js
@@ -0,0 +1,9 @@
+
+function HelloWorld(){
+
+    return <h1>Hello World!</h1>
+}
+
+ReactDOM.render(
+    <HelloWorld/>, document.getElementById('mydiv')
+);
