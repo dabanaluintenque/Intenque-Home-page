d68413589b6097770bba0f8ed84e29f024744427
Dabana Intenque <64395601+dabanaluintenque@users.noreply.github.com>
2023-07-27 15:34:00 -0400

    add path

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public/Tutorial')));
-app.use(express.static(path.join(__dirname, 'Tutorial')))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/Tutorial')))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
