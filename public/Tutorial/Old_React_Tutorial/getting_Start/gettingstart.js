e4d6cf8327d79f68e6584f3a569b71fdb5a6a7cf
Dabana Intenque <64395601+dabanaluintenque@users.noreply.github.com>
2023-07-27 14:07:53 -0400

    fixing the router error in nodejs index.js

--- public/Tutorial/Old_React_Tutorial/getting_Start/gettingstart.js
+++
@@ -1,9 +0,0 @@
-
-function HelloWorld(){
-
-    return <h1>Hello World!</h1>
-}
-
-ReactDOM.render(
-    <HelloWorld/>, document.getElementById('mydiv')
-);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
+var path = require('path');
 
-/* GET home page. */
+/* GET home page. 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});*/
+
+app.use(express.static(path.join(__dirname,"./my-homepage")));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, './my-homepage', 'index.html'))
 });
 
 module.exports = router;
