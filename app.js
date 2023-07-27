67b6deb3aeb4d8159e16b129fa7832c27575f514
Dabana Intenque <64395601+dabanaluintenque@users.noreply.github.com>
2023-07-27 15:49:35 -0400

    add path

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "public/Tutorial", "index.html"));
 });
 
